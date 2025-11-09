059307818564a2f0bf465d3c1c5391f41ccd7ee3
Aljaž Mur Eržen <aljaz@erzen.si>
2025-11-09 17:38:31 +0100

    nix env

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      fenix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        fenix_pkgs = fenix.packages.${system};
+        rust_toolchain = fenix_pkgs.stable;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          venvDir = "./.venv";
+
+          buildInputs = with pkgs; [
+            python312Packages.python
+            python312Packages.venvShellHook
+            python312Packages.python-lsp-server
+            python312Packages.python-lsp-ruff
+            uv
+
+            maturin
+
+            (rust_toolchain.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+              "rust-analyzer"
+            ])
+
+          ];
+        };
+      }
+    );
+}
